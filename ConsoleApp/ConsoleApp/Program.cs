d2fb705a088acf7d236cb5f48cb287c93aa9ec90
Mamedali26 <mamed.gasymov@gmail.com>
2020-10-01 19:09:42 +0400

    Added files

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, Student> students = new Dictionary<int, Student>();
+            students.Add(1, new Student("Gasimov", "Mathematics", 4219));
+            students.Add(2, new Student("Alizade", "Physics", 4005));
+            students.Add(3, new Student("Huseynov", "History", 2683));
+
+            students[4] = new Student("Balayev", "Chemistry", 1901);
+            students[5] = new Student("Azizov", "Literature", 3654);
+            students[6] = new Student("Maharramov", "Computer Science", 4219);            
+
+            foreach(KeyValuePair<int, Student> p in students)
+            {
+                Console.Write($"{p.Key} - ");
+                p.Value.Print();
+            }
+
+            Console.WriteLine($"Количество студентов: {students.Count}.");
+            Console.WriteLine("----------------------------------------\n");
+            students.Remove(6);
+            students.Remove(5);
+            foreach (KeyValuePair<int, Student> p in students)
+            {
+                Console.Write($"{p.Key} - ");
+                p.Value.Print();
+            }
+
+            Console.WriteLine($"Количество студентов после удаления нескольких студентов: {students.Count}.");
+            Console.WriteLine("----------------------------------------\n");
+
+            if (students.ContainsKey(4))
+                students[4].Print();
+            else Console.WriteLine("Список не содержит студента с номером 4!");
+            Console.WriteLine("----------------------------------------\n");
+
+            students.Clear();
+            Console.WriteLine($"Количество студентов после очистки списка: {students.Count}. Список пуст...");
+            Console.WriteLine("----------------------------------------\n");
+        }
+    }
+}
---
+++ ConsoleApp/ConsoleApp/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp
+{
+    class Student
+    {
+        public string LastName { get; set; }
+        public string Course { get; set; }
+        public int StudentBook { get; set; }
+
+        public string UpLastName;
+        public string UpCourse;
+
+        public Student(string lastName, string course, int id)
+        {
+            LastName = lastName;
+            Course = course;
+            StudentBook = id;
+            UpLastName = LastName[0].ToString().ToUpper() + LastName.Substring(1);
+            UpCourse = Course[0].ToString().ToUpper() + Course.Substring(1);
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"Студент {UpLastName} изучает {UpCourse} и номер зачетной книжки равен {StudentBook}.");
+        }
+
+    }
+}
